0ca44a973b073f53397cb531ba453bd423258a3d
Dmitry Vergelsky <graf.werger@gmail.com>
2023-12-16 16:23:40 +0700

    Создал структуру main.py и BDManager

--- api_hh.py
+++ api_hh.py
@@ -2,11 +2,11 @@ import requests
 
 
 class VacanciesFromEmployer():
-    max_qty_vacancies = 100
+    qty_vacancies = 20
     api_url = "https://api.hh.ru/vacancies/"
 
-    def load_vacancies(self, employer_id):
+    def get_vacancies(self, employer_id):
         """
         Загружает с hh.ru и возвращает список вакансий работодателя в json
         """
-        response = requests.get(self.api_url, params={"per_page": self.max_qty_vacancies, "employer_id": employer_id})
+        response = requests.get(self.api_url, params={"per_page": self.qty_vacancies, "employer_id": employer_id})
--- main.py
+++ main.py
@@ -6,10 +6,49 @@ employers_file = "employers.json"
 
 
 def start_work():
-    with open(employers_file) as file:
+    #получаем список избранных компаний
+    with open(employers_file, encoding='UTF-8') as file:
         employers_list = json.load(file)
-    print(employers_list)
 
+    #создаём базу данных
+    bdm = BDManager()
 
-if __name__ == "main.py":
-    start_work()
+    vfe = VacanciesFromEmployer()
+
+    #для каждого работодателя
+    for employer in employers_list:
+        #загружаем вакансии
+        vacancies = vfe.get_vacancies(employer)
+        for vacancy in vacancies:
+            #и записываем в бд
+            bdm.add_note(vacancy)
+    #возвращаем бд для дальнейшей работы
+    return bdm
+
+if __name__ == "__main__":
+    bdm = start_work()
+
+    while True:
+        responce = input("Введите команду:\n"
+                         "СК - Список всех Компаний,\n"
+                         "СВ - Список всех Вакансий,\n"
+                         "СЗ - Средняя Зарплата по всем вакансиям,\n"
+                         "ВС - вакансии с зарплатой Выше Средней,\n"
+                         "П профа - Поиск вакансий со словом \"профа\" в названии,\n"
+                         "В - завершить работу программы.")
+
+        match responce.split(" "):
+            case ["СК"]:
+                bdm.get_companies_and_vacancies_count()
+            case ["СВ"]:
+                bdm.get_all_vacancies()
+            case ["СЗ"]:
+                bdm.get_avg_salary()
+            case ["ВС"]:
+                bdm.get_rich_vacancies()
+            case ["П"], *name:
+                bdm.search_in_vacancies(name)
+            case ["В"]:
+                quit()
+            case bad_response:
+                print(f"Команда {bad_response} не существует, повторите ввод.")
